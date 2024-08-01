a6e00cac6f030630852f9f5a88e7cf97735ff00a
Yunfei <i.heyunfei@gmail.com>
2024-08-01 23:31:06 +0800

    Impl

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@
  * The entrypoint for the action.
  */
 const { run } = require('./main')
-
 run()
--- src/main.js
+++ src/main.js
@@ -1,30 +1,69 @@
+const github = require('@actions/github')
 const core = require('@actions/core')
-const { wait } = require('./wait')
+const R = require('remeda')
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds', { required: true })
+exports.run = async function run() {
+  // This should be a token with access to your repository scoped in as a secret.
+  // The YML workflow will need to set myToken with the GitHub Secret Token
+  // myToken: ${{ secrets.GITHUB_TOKEN }}
+  // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+  const githubToken = process.env.GITHUB_TOKEN
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+  const allowNoAssign = true
+  const context = github.context
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    core.setFailed(error.message)
+  if (!githubToken) {
+    core.setFailed("Can't get `GITHUB_TOKEN` from environment.")
+    return
   }
-}
 
-module.exports = {
-  run
+  if (context.eventName !== 'pull_request' || !context.payload.pull_request) {
+    return
+    // Assignee is only meaningful on PRs
+  }
+
+  const octokit = github.getOctokit(githubToken)
+
+  // You can also pass in additional options as a second parameter to getOctokit
+  // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
+  const { data: reviews } = await octokit.rest.pulls.listReviews()
+
+  const approvers = new Set(
+    R.pipe(
+      reviews,
+      R.filter(review => review.state === 'APPROVED'),
+      R.map(review => review.user?.login ?? null),
+      R.filter(R.isNonNull)
+    )
+  )
+
+  const { data: pullRequest } = await octokit.rest.pulls.get({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: context.payload.pull_request.number,
+    mediaType: {
+      format: 'diff'
+    }
+  })
+
+  if (!pullRequest.assignees) {
+    core.setFailed("Can't get assignees from pull request.")
+    return
+  }
+
+  if (pullRequest.assignees.length === 0 && !allowNoAssign) {
+    core.setFailed('No assignees found on pull request.')
+    return
+  }
+
+  const assigneesNotApproved = pullRequest.assignees.filter(assignee =>
+    approvers.has(assignee.login)
+  )
+
+  if (assigneesNotApproved.length > 0) {
+    core.setFailed(
+      `Require assignees ${assigneesNotApproved.map(assignee => assignee.login).join(', ')} to approved the PR.`
+    )
+    return
+  }
 }
